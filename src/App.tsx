d5296d11fd7dead0dc7d32b625995bf0c9b791b8
Genevieve Coogan <gcoogan1@gmail.com>
2025-03-10 15:31:06 -0400

    Initial setup - install dependencies, add assets and theme.

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+	return (
+		<>
+			<h1>Groupcation - To The Alps</h1>
+		</>
+	);
+};
+
+export default App;
---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+export const theme = {
+  primary: '#E40078',
+  secondary: '#404040',
+  surface: '#F4F4F4',
+  line: '#EAEAEA',
+  stay: '#009DEC',
+  flight: '#6F00FF',
+  train: '#AA00E2',
+  bus: '#005A8E',
+  boat: '#5C79EB',
+  rental: '#FFA100',
+  activity: '#59AD05',
+  event: '#FF0856',
+  celebration: '#E40078',
+  walking: '#BD8E62',
+  driving: '#DB8A26'
+}
